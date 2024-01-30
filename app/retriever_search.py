c4729e08b09763d9d04259827f174e76b20abbec
goyalpramod <goyalpramod1729@gmail.com>
2024-01-30 18:33:37 +0530

    Fix return type in retrieve_relevant_docs

--- app/retriever_search.py
+++ app/retriever_search.py
@@ -2,13 +2,15 @@ from app.vectorstores import InitialisePinecone
 from app.chain import fetch_data
 from langchain_community.vectorstores import Pinecone as PineconeVectorstore
 from langchain_openai import OpenAIEmbeddings
+from typing import List, Tuple
+from langchain_core.documents.base import Document
 
 index_name = "pod-index"
 
-def retrieve_relevant_docs(query:str = "Perform UAT") -> dict:
+def retrieve_relevant_docs(query:str = "add new search functionality") -> List[Tuple[Document, float]]:
     """
     :param: str
-    :return: dict
+    :return: List[Tuple[Document, float]]
     takes in the query and responds with the relevant documents present in the database
     """
     pinecone = InitialisePinecone()
