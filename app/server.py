20d640a7e533bd8c6c34348f5a3b3a4368ea3a7a
goyalpramod <goyalpramod1729@gmail.com>
2024-01-31 21:44:52 +0530

    Add chain retrieval route to server.py

--- app/server.py
+++ app/server.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from app.chain import chain as check_duplicate_chain
+from app.chain import MakeChain
+
+chain_obj = MakeChain()
+chain_obj.choose_embeddings()
+chain_obj.choose_vectorstore()
+retriever = chain_obj.get_retriever()
 
 app = FastAPI()
 
@@ -13,6 +19,7 @@ async def redirect_root_to_docs():
 
 # Edit this to add the chain you want to add
 add_routes(app, check_duplicate_chain, path="/check-duplicate")
+add_routes(app, retriever, path="/retrieve-relevant-docs")
 
 if __name__ == "__main__":
     import uvicorn
