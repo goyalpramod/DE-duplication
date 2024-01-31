2da7081101891955fc41f3fc6dc55cb47c4b7422
goyalpramod <goyalpramod1729@gmail.com>
2024-01-31 16:11:08 +0530

    Refactor retriever_search.py to improve search functionality

--- app/retriever_search.py
+++ app/retriever_search.py
@@ -1,22 +1,18 @@
-from app.vectorstores import InitialisePinecone
-from app.chain import fetch_data
 from langchain_community.vectorstores import Pinecone as PineconeVectorstore
 from langchain_openai import OpenAIEmbeddings
 from typing import List, Tuple
 from langchain_core.documents.base import Document
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
-index_name = "pod-index"
-
-def retrieve_relevant_docs(query:str = "add new search functionality") -> List[Tuple[Document, float]]:
+def retrieve_relevant_docs(query:str) -> List[Tuple[Document, float]]:
     """
     :param: str
     :return: List[Tuple[Document, float]]
     takes in the query and responds with the relevant documents present in the database
     """
-    pinecone = InitialisePinecone()
-    pinecone.make_index()
+    index_name = "pod-index"
     embeddings = OpenAIEmbeddings()
-    docs = fetch_data({"question" : query})
-    docsearch = PineconeVectorstore.from_documents(docs["context"], embeddings, index_name=index_name)
-    docs = docsearch.similarity_search_with_relevance_scores(docs["question"])
+    docsearch = PineconeVectorstore.from_existing_index(index_name = index_name, embedding = embeddings)
+    docs = docsearch.similarity_search_with_relevance_scores(query)
     return docs
