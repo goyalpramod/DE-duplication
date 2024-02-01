7a1cbcdd697b425d6a77404ff4a310f9d0f9f21f
goyalpramod <goyalpramod1729@gmail.com>
2024-02-01 13:15:16 +0530

    Add search_duplicate_chain to server.py

--- app/server.py
+++ app/server.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from app.chain import chain as check_duplicate_chain
+from langchain_core.runnables import RunnableLambda
 from app.chain import MakeChain
+from app.main import search_duplicate
 
 chain_obj = MakeChain()
 chain_obj.choose_embeddings()
 chain_obj.choose_vectorstore()
-retriever = chain_obj.get_retriever()
+retriever_chain = chain_obj.get_retriever()
 
 app = FastAPI()
 
+search_duplicate_chain = RunnableLambda(search_duplicate)
 
 @app.get("/")
 async def redirect_root_to_docs():
@@ -19,7 +22,8 @@ async def redirect_root_to_docs():
 
 # Edit this to add the chain you want to add
 add_routes(app, check_duplicate_chain, path="/check-duplicate")
-add_routes(app, retriever, path="/retrieve-relevant-docs")
+add_routes(app, retriever_chain, path="/retrieve-relevant-docs")
+add_routes(app, search_duplicate_chain, path="/search-duplicate")
 
 if __name__ == "__main__":
     import uvicorn
